35e93c3c487e565b84709661cc598897c3c532f6
Connor <connorskorburg@gmail.com>
2020-05-25 17:46:00 -0500

    initial commit

---
+++ playlistofchoice/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.home),
+    path('search_track', views.search_track),
+    path('track_results', views.track_results),
+    path('new_releases', views.new_releases),
+]
